a8d19568c9beb9ccfd70985afb2a87eb6f0870f7
shayansh06 <shayan06bagherzadeh@gmail.com>
2021-06-04 18:11:03 +0430

    Update Average.py

--- Average.py
+++ Average.py
@@ -5,6 +5,8 @@ user_number = int(input('enter your number:'))
 while user_number != -1:
     numbers.append(user_number)
     user_number = int(input('enter your next number:'))
+    
+#when you type (-1) , it will stop adding numbers to the list and will show the result
 
 
 user_next_number = 0
